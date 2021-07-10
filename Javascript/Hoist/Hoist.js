bc08ef4cc651f4c4bc9050413a0c73f973c02e5a
Adam Dalbah <adamdalbah@gmail.com>
2021-07-10 19:01:20 +0300

    Hoist assignment done

---
+++ Javascript/Hoist/Hoist.js
@@ -0,0 +1,105 @@
+//1- 
+// var hello;
+// console.log(hello);
+// hello = 'world';
+
+//----------------------------
+
+//2- 
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+// var needle = 'haystack';
+// test();
+
+//----------------------------
+
+//3-
+// function print(){
+//     var brendan;
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// var brendan = 'super cool';
+// console.log(brendan); 
+
+//----------------------------
+
+//4-
+// function eat() {
+//     var food;
+//     food = 'half-chicken';
+//     console.log(food);
+//     food = 'gone';
+// }
+// var food = 'chicken';
+// console.log(food);
+// eat();
+
+//----------------------------
+
+//5-
+// var mean;
+// mean();
+// console.log(food);
+// mean = function(){
+//     var food;
+//     food = "chicken";
+//     console.log(food);
+//     food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+//----------------------------
+
+//6-
+// function rewind(){
+//     var genre;
+//     genre = "rock";
+//     console.log(genre);
+//     genre = "r&b";
+//     console.log(genre);
+// }
+// var genre;
+// console.log(genre);
+// genre = "disco";
+// rewind();
+// console.log(genre);
+
+//----------------------------
+
+//7-
+// function learn(){
+//     var dojo;
+//     dojo = "seattle";
+//     console.log(dojo);
+//     dojo = "burbank";
+//     console.log(dojo);
+// }
+// var dojo;
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// console.log(dojo);
+
+//----------------------------
+
+//8-
+// function makeDojo(name, students){
+//     const dojo = {};
+//     dojo.name = name;
+//     dojo.students = students;
+//     if(dojo.students > 50){
+//         dojo.hiring = true;
+//     }
+//     else if(dojo.students <= 0){
+//         dojo = "closed for now";
+//     }
+//     return dojo;
+// }
+// console.log(makeDojo("Chicago", 65));
+// console.log(makeDojo("Berkeley", 0));
+
+
