20ffc50ec913a65a64d9d5374f189ef98de68058
Adam Dalbah <adamdalbah@gmail.com>
2021-07-14 08:23:16 +0300

    increase bithday

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            counter:0
+        }
+    }
     render(){
         const {firstName, lastName, age, hairColor} = this.props;
+        const{counter} = this.state.counter;
         return (
             <>
                 <h1>{firstName}, {lastName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {age + this.state.counter}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={ this.increase }>Birthday Button for {firstName} {lastName}</button>
             </>
             )
         }
+    increase = () => {
+        let count  = this.state.counter + 1;
+        this.setState({counter: count});
+    }   
         
 }
 export default PersonCard;
