695f8a3edcb2191325e41ceacf8840787601d05d
Adam Dalbah <adamdalbah@gmail.com>
2021-08-01 10:09:32 +0300

    product Manger part ll

--- Fullstack_mern/productManger/server/routes/product.routes.js
+++ Fullstack_mern/productManger/server/routes/product.routes.js
@@ -1,4 +1,6 @@
 const ProductController = require('../controllers/product.controllers');
 module.exports = function(app) {
     app.post('/api/products/new', ProductController.createProduct);
+    app.get('/api/products', ProductController.findAllProducts);
+    app.get('/api/products/:id', ProductController.findProduct);
 }
